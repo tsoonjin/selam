2e465d3a709896acd3432eafb0f000e004d266c4
jinified <jinified@gmail.com>
2017-03-09 13:35:34 +0800

    Added MSER detection

--- examples/propose_objects.py
+++ examples/propose_objects.py
@@ -6,6 +6,18 @@ from selam.utils import img
 from selam.enhancement import shadegrey
 
 
+def mserSearch(img):
+    """ Using different thresholds on grayscale image to detect different components """
+    mser = cv2.MSER_create()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)[..., 1]
+    regions, _ = mser.detectRegions(gray)
+    hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions]
+    cv2.polylines(img, hulls, 1, (0, 255, 0))
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 def selective_search(img):
     """ Performs selective search and draw all detected regions """
     labels, regions = ssearch.selective_search(img, scale=10, sigma=0.9, min_size=10)
@@ -17,10 +29,10 @@ def selective_search(img):
 
 
 def main():
-    path = './examples/dataset/robosub16/SEMI/677-1226_tower_table'
+    path = './examples/dataset/robosub16/SEMI/4142-4424_tower_bright'
     imgs = img.get_jpgs(path, resize=2)
-    imgs = map(lambda i: cv2.cvtColor(i, cv2.COLOR_BGR2RGB), imgs)
-    chosen = shadegrey(imgs[0])
+    chosen = shadegrey(imgs[31])
+    mserSearch(chosen)
 
 
 if __name__ == '__main__':
