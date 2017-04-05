b2751cfcd48653e26735e20f73c1ab354ec7690e
jinified <jinified@gmail.com>
2017-04-05 13:28:21 +0800

    Final push

--- report/auto/main.el
+++ report/auto/main.el
@@ -2,7 +2,7 @@
  "main"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("socreport" "fypca")))
+                     '(("socreport" "hyp")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("enumitem" "shortlabels") ("inputenc" "utf8")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
@@ -18,10 +18,13 @@
     "chap0_abstract"
     "socreport"
     "socreport10"
+    "mathtools"
     "fullpage"
     "float"
     "hyperref"
     "graphicx"
+    "amsmath"
+    "caption"
     "enumitem"
     "inputenc")
    (LaTeX-add-labels
@@ -33,7 +36,28 @@
     "fig:main_methodology"
     "fig:dataset_methodology"
     "fig:training_methodology"
-    "fig:tracking_methodology")
+    "fig:tracking_methodology"
+    "fig:colorconstancy_results"
+    "fig:color_gamma"
+    "fig:grey_pixel"
+    "fig:spatial_colorconstancy"
+    "fig:underwater_image_enhancement_results"
+    "fig:fusion_pipeline"
+    "fig:illumination_compensation_results"
+    "fig:light_compensation"
+    "fig:proposal_results"
+    "fig:mser_proposal"
+    "fig:salient_proposal"
+    "fig:similar_color"
+    "sec:color_descriptors"
+    "sec:shape_descriptors"
+    "fig:inner_shapecontext"
+    "fig:gp"
+    "fig:tracker"
+    "fig:dataset1"
+    "fig:dataset2"
+    "table:competing_trackers"
+    "table:raw_results")
    (LaTeX-add-bibliographies
     "fyp"))
  :latex)
